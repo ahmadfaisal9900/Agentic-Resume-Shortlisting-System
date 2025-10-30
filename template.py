29dc3f8f5dabc2bf9d046949004d7691b69dd14c
Ahmad Faisal <ahmadfaisal9900@gmail.com>
2025-10-30 11:35:33 +0500

    project structure

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%asctime)s]: %(message)s:')
 
-project_name = "ChestCancerClassification"
+project_name = "AgenticHR"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
